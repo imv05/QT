f29df1d48f10ae3c096049a90dd807573fe86004
R Z <2420398231@qq.com>
2024-06-30 14:10:24 +0800

    加入超链接

--- subway/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/ui_menu.h
+++ subway/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/ui_menu.h
@@ -20,42 +20,34 @@ QT_BEGIN_NAMESPACE
 class Ui_menu
 {
 public:
-    QPushButton *pushButton;
-    QLabel *label_2;
-    QPushButton *pushButton_2;
-    QLabel *label_3;
+    QPushButton *enterButton;
+    QLabel *authors;
+    QPushButton *exitButton_2;
 
     void setupUi(QWidget *menu)
     {
         if (menu->objectName().isEmpty())
             menu->setObjectName("menu");
         menu->resize(876, 572);
-        pushButton = new QPushButton(menu);
-        pushButton->setObjectName("pushButton");
-        pushButton->setGeometry(QRect(200, 230, 191, 71));
+        enterButton = new QPushButton(menu);
+        enterButton->setObjectName("enterButton");
+        enterButton->setGeometry(QRect(200, 230, 191, 71));
         QFont font;
         font.setPointSize(30);
-        pushButton->setFont(font);
-        pushButton->setCursor(QCursor(Qt::CursorShape::PointingHandCursor));
-        label_2 = new QLabel(menu);
-        label_2->setObjectName("label_2");
-        label_2->setGeometry(QRect(190, 160, 501, 51));
+        enterButton->setFont(font);
+        enterButton->setCursor(QCursor(Qt::CursorShape::PointingHandCursor));
+        authors = new QLabel(menu);
+        authors->setObjectName("authors");
+        authors->setGeometry(QRect(190, 160, 501, 51));
         QFont font1;
         font1.setPointSize(22);
-        label_2->setFont(font1);
-        pushButton_2 = new QPushButton(menu);
-        pushButton_2->setObjectName("pushButton_2");
-        pushButton_2->setGeometry(QRect(510, 230, 191, 71));
-        pushButton_2->setFont(font);
-        pushButton_2->setCursor(QCursor(Qt::CursorShape::PointingHandCursor));
-        pushButton_2->setFlat(false);
-        label_3 = new QLabel(menu);
-        label_3->setObjectName("label_3");
-        label_3->setGeometry(QRect(0, 0, 871, 571));
-        label_3->raise();
-        pushButton->raise();
-        label_2->raise();
-        pushButton_2->raise();
+        authors->setFont(font1);
+        exitButton_2 = new QPushButton(menu);
+        exitButton_2->setObjectName("exitButton_2");
+        exitButton_2->setGeometry(QRect(510, 230, 191, 71));
+        exitButton_2->setFont(font);
+        exitButton_2->setCursor(QCursor(Qt::CursorShape::PointingHandCursor));
+        exitButton_2->setFlat(false);
 
         retranslateUi(menu);
 
@@ -65,10 +57,9 @@ public:
     void retranslateUi(QWidget *menu)
     {
         menu->setWindowTitle(QCoreApplication::translate("menu", "Form", nullptr));
-        pushButton->setText(QCoreApplication::translate("menu", "\350\277\233\345\205\245", nullptr));
-        label_2->setText(QCoreApplication::translate("menu", "--by \345\210\230\345\215\223\346\264\213\357\274\214\350\265\265\347\221\236\346\215\267\357\274\214\350\260\242\345\256\207\347\277\224", nullptr));
-        pushButton_2->setText(QCoreApplication::translate("menu", "\351\200\200\345\207\272", nullptr));
-        label_3->setText(QString());
+        enterButton->setText(QCoreApplication::translate("menu", "\350\277\233\345\205\245", nullptr));
+        authors->setText(QCoreApplication::translate("menu", "--by \345\210\230\345\215\223\346\264\213\357\274\214\350\265\265\347\221\236\346\215\267\357\274\214\350\260\242\345\256\207\347\277\224", nullptr));
+        exitButton_2->setText(QCoreApplication::translate("menu", "\351\200\200\345\207\272", nullptr));
     } // retranslateUi
 
 };
--- subway/menu.cpp
+++ subway/menu.cpp
@@ -1,18 +1,46 @@
 #include "menu.h"
 #include "ui_menu.h"
+#include <QDesktopServices>
 MainWindow* menu::mw;
 //创建了一个新的窗口（主界面）
+
+class UrlLabel: public QLabel{
+    // Q_OBJECT
+
+public:
+    UrlLabel(const QUrl &url, QWidget *parent = nullptr) : QLabel(parent), url_(url) {
+        // 设置标签为空文本，或者设置为透明背景的图片等
+        // setText("");
+        // 你可以设置固定的尺寸，或者根据需要进行调整
+        // setFixedSize(100, 50); // 示例尺寸
+        // 设置样式，例如背景色等
+        setStyleSheet("background-image:url(:/images/src/combined.png);");
+        setAlignment(Qt::AlignCenter);
+    }
+
+protected:
+    QUrl url_;
+    void mousePressEvent(QMouseEvent *event) override{
+        // QLabel::mousePressEvent(event);
+        // 打开网页
+        QDesktopServices::openUrl(url_);
+    }
+};
+
 menu::menu(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::menu)
 {
     ui->setupUi(this);
-    ui->label_3->setScaledContents(true);
-    ui->label_3->setAlignment(Qt::AlignCenter);
-    // ui->label->setStyleSheet("color:red");
 
-    ui->label_3->setStyleSheet("background-image:url(:/images/src/combined.png);");
-
+    // ui->label_3->setScaledContents(true);
+    // ui->label_3->setAlignment(Qt::AlignCenter);
+    // ui->label_3->setStyleSheet("background-image:url(:/images/src/combined.png);");
+    QString url = "https://github.com/imv05/QT";
+    QLabel* background = new UrlLabel(url, this);
+    background->setGeometry(1,0,875,572);
+    background->lower();
+    // qDebug() << children();
 }
 
 menu::~menu()
